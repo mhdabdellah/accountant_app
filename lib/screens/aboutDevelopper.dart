8da69ea617be65f3a8b33da3e06c06567c5f3e5c
mohamedabdellahiSoft <162436764+mohamedabdellahiSoft@users.noreply.github.com>
2024-03-26 14:08:07 +0000

    gg

--- lib/screens/aboutDevelopper.dart
+++ lib/screens/aboutDevelopper.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:pdfx/pdfx.dart';
+// import 'package:pdfx/pdfx.dart';
 
 class AboutDeveloper extends StatelessWidget {
-  AboutDeveloper({super.key});
+  const AboutDeveloper({super.key});
 
-  final pdfController = PdfController(
-    document: PdfDocument.openAsset('assets/mhdabdellahi_2023-2024_cv.pdf'),
-  );
+  // final pdfController = PdfController(
+  //   document: PdfDocument.openAsset('assets/mhdabdellahi_2023-2024_cv.pdf'),
+  // );
 
   @override
   Widget build(BuildContext context) {
-    return PdfView(
-      controller: pdfController,
-    );
+    return Container();
+    // PdfView(
+    //   controller: pdfController,
+    // );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,18 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import device_info_plus_macos
 import path_provider_foundation
-import pdfx
 import shared_preferences_foundation
 import sign_in_with_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
